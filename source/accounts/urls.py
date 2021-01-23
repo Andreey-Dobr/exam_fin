d87016b1b97e31aa7601579c98f9b9c53210641e
none <none@myhost>
2021-01-23 10:24:19 +0600

    добавлена регистрация

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 #from accounts.views import RegisterView, RegisterActivateView, UserDetailView, \
     #UserChangeView, UserPasswordChangeView, UserPasswordResetEmailView, UserPasswordResetView
+from accounts.views import BaseView, RegisterView
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
+    path('', BaseView.as_view(), name='base'),
+    #path('login/', LoginView.as_view(), name='login'),
     #path('logout/', LogoutView.as_view(), name='logout'),
-    #path('register/', RegisterView.as_view(), name='create'),
+    path('register/', RegisterView.as_view(), name='create'),
     #path('activate/<uuid:token>/', RegisterActivateView.as_view(), name='activate'),
     #path('<int:pk>/', UserDetailView.as_view(), name='detail'),
     #path('<int:pk>/update/', UserChangeView.as_view(), name='change'),
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.views.generic import DetailView, TemplateView, CreateView
+from django.urls import reverse
+from django.shortcuts import redirect
 
-# Create your views here.
+from accounts.forms import MyUserCreationForm
+
+
+class RegisterView(CreateView):
+    model = User
+    template_name = 'user_create.html'
+    form_class = MyUserCreationForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('accounts:base')
+        return next_url
+
+class BaseView(TemplateView):
+
+    template_name = 'index.html'
